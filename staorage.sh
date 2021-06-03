f3c234dc49d4d1e92d51e5afe22201304d9b29c8
Prasanjit Swain <57293437+pswainuu@users.noreply.github.com>
2021-06-03 12:30:33 +0530

    Update staorage.sh

--- staorage.sh
+++ staorage.sh
@@ -1,2 +1,2 @@
-gcloud asset search-all-iam-policies --flatten="[], [].policy.bindings[].members[]" --format="csv(project,resource,policy.bindings.members.basename(),policy.bindings.role)"
-gsutil mb gs://testing450/
+gcloud asset search-all-iam-policies --flatten="[], [].policy.bindings[].members[]" --format="csv(project,resource,policy.bindings.members.basename(),policy.bindings.role)" --output-path=gs://testing450/test.csv
+
